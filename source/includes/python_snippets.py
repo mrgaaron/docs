caea535fc46e0a414d66455a962041d69b3d0f44
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2015-01-22 13:38:31 -0600

    snippets for stream binding and device ordering

--- source/includes/python_snippets.py
+++ source/includes/python_snippets.py
@@ -45,3 +45,48 @@
         print("Found point: t={} v={}".format(point_time, point_value))
 
 # snippet-end
+# snippet-begin bind-single-stream
+
+    result = client.query(Sensor) \
+                   .filter(Device.key == "device1") \
+                   .read(start=datetime.datetime(2015, 1, 1),
+                         end=datetime.datetime(2015, 1, 1))
+    stream1 = result.data.bind_one(sensor_key='temperature')
+    for point in stream1:
+        print "Temperature: t={} v={}".format(point.timestamp, point.value)
+
+    stream2 = result.data.bind_one(sensor_key='humidity')
+    for point in stream2:
+        print "Humidity: t={} v={}".format(point.timestamp, point.value)
+
+
+# snippet-end
+# snippet-begin bind-all-streams
+
+    result = client.query(Sensor) \
+                   .filter(Device.key == "device1") \
+                   .read(start=datetime.datetime(2015, 1, 1),
+                         end=datetime.datetime(2015, 1, 1))
+
+    for stream in result.data.streams:
+        for point in stream:
+            print "Device: {}, sensor: {}".format(stream.device.key,
+                                                  stream.sensor.key)
+            print "Point: t={} v={}".format(point.timestamp, point.value)
+
+# snippet-end
+# snippet-begin device-ordering-key
+
+    result = client.query(Device).order_by('key', 'desc').read()
+
+# snippent-end
+# snippet-begin device-ordering-date-created
+
+    result = client.query(Device).order_by('date_created', 'asc').read()
+
+# snippent-end
+# snippet-begin device-ordering-date-modified
+
+    result = client.query(Device).order_by('date_modified', 'desc').read()
+
+# snippent-end
